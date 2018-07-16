0be9dde538d5bc05a32df9eaa2e71fbd5c750e0d
中安佑一 <yuichi.nakayasu@gmail.com>
2018-07-16 22:22:03 +0900

    路線コードから路線を取得する実装

--- Itadaki/Sources/Model/Repository/RailwayRepository.swift
+++ Itadaki/Sources/Model/Repository/RailwayRepository.swift
@@ -6,4 +6,8 @@ import Foundation
 
 class RailwayRepository {
     
+    class func code(_ code: String) -> Railway? {
+        let predicate = NSPredicate("code", equal: code)
+        return Realm.select(from: Railway.self, predicate: predicate).first
+    }
 }
