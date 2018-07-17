604a02851afe6056004eb13fe2c1277bc5bf1c29
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2018-07-17 17:32:30 +0900

    IndexPathの拡張追加

---
+++ Itadaki/Sources/Extension/IndexPath+App.swift
@@ -0,0 +1,12 @@
+// =============================================================================
+//  Itadaki
+//  Copyright Yuichi Nakayasu. All rights reserved.
+// =============================================================================
+import UIKit
+
+extension IndexPath {
+    
+    init(_ row: Int, in section: Int = 0) {
+        self.init(row: row, section: section)
+    }
+}
