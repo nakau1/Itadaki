b4ce1c6c710a91970e3a5b46bbfa109e2e54cd6f
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2018-07-17 17:33:14 +0900

    リスト選択用のコントロールを追加

--- Itadaki/Sources/Definition/Notification.swift
+++ Itadaki/Sources/Definition/Notification.swift
@@ -8,6 +8,9 @@ extension Notification.Name {
     
     static let CommandForward = Notification.Name("CommandForward")
     static let CommandTransfer = Notification.Name("CommandTransfer")
+    static let CommandListSelect = Notification.Name("CommandListSelect")
+    static let CommandListUp = Notification.Name("CommandListUp")
+    static let CommandListDown = Notification.Name("CommandListDown")
     
     static let WillStationMove = Notification.Name("WillStationMove")
     static let DidStationMove = Notification.Name("DidStationMove")
