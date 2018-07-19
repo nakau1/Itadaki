3f7cad57183c4cc44c8cb3082e67605dfb969b62
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2018-07-20 00:00:34 +0900

    リストビューのベースを作成

--- Itadaki/Sources/Screen/GameTransferringSelect/GameTransferringSelectAdapter.swift
+++ Itadaki/Sources/Screen/GameTransferringSelect/GameTransferringSelectAdapter.swift
@@ -14,75 +14,87 @@ class GameTransferringSelectAdapter: NSObject {
     
     weak var delegate: GameTransferringSelectAdapterDelegate!
     
-    @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet private weak var listView: ListView!
     
     private(set) var currentIndex = -1
     
     func reload() {
-        tableView.isScrollEnabled = false
-        tableView.reloadData()
-        if delegate.numberOfItems(transferSelectAdapter: self) > 0 {
-            currentIndex = 0
-            tableView.selectRow(at: IndexPath(currentIndex), animated: false, scrollPosition: .top)
-        }
+//        tableView.isScrollEnabled = false
+//        tableView.reloadData()
+//        if delegate.numberOfItems(transferSelectAdapter: self) > 0 {
+//            currentIndex = 0
+//            tableView.selectRow(at: IndexPath(currentIndex), animated: false, scrollPosition: .top)
+//        }
     }
     
     func selectUp() {
-        if delegate.numberOfItems(transferSelectAdapter: self) <= 0 || currentIndex <= 0 { return }
-        currentIndex -= 1
-        
-        let indexPath = IndexPath(currentIndex)
-        if !isVisibleRows(at: indexPath) {
-            tableView.scrollToRow(at: indexPath, at: .middle, animated: true)
-        }
-        tableView.reloadData()
+//        if delegate.numberOfItems(transferSelectAdapter: self) <= 0 || currentIndex <= 0 { return }
+//        currentIndex -= 1
+//
+//        let indexPath = IndexPath(currentIndex)
+//        if !isVisibleRows(at: indexPath) {
+//            tableView.scrollToRow(at: indexPath, at: .middle, animated: true)
+//        }
+//        tableView.reloadData()
     }
     
     func selectDown() {
-        if currentIndex >= delegate.numberOfItems(transferSelectAdapter: self) - 1 { return }
-        currentIndex += 1
-        
-        let indexPath = IndexPath(currentIndex)
-        if !isVisibleRows(at: indexPath) {
-            tableView.scrollToRow(at: indexPath, at: .middle, animated: true)
-        }
-        tableView.reloadData()
+//        if currentIndex >= delegate.numberOfItems(transferSelectAdapter: self) - 1 { return }
+//        currentIndex += 1
+//
+//        let indexPath = IndexPath(currentIndex)
+//        if !isVisibleRows(at: indexPath) {
+//            tableView.scrollToRow(at: indexPath, at: .middle, animated: true)
+//        }
+//        tableView.reloadData()
     }
     
     private func isVisibleRows(at indexPath: IndexPath) -> Bool {
-        let indexPaths = tableView.indexPathsForVisibleRows ?? []
-        return indexPaths.dropFirst(2).dropLast(2).contains(indexPath)
+        return false
+//        let indexPaths = tableView.indexPathsForVisibleRows ?? []
+//        return indexPaths.dropFirst(2).dropLast(2).contains(indexPath)
     }
 }
 
-extension GameTransferringSelectAdapter: UITableViewDelegate, UITableViewDataSource {
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+extension GameTransferringSelectAdapter: ListViewDelegate, ListViewDatasource {
+    
+    func numberOfRows(in listView: ListView) -> Int {
         return delegate.numberOfItems(transferSelectAdapter: self)
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! GameTransferringSelectAdapterCell
-        cell.transferring = delegate.transferSelectAdapter(self, transferringAt: indexPath.row)
-        cell.current = currentIndex == indexPath.row
-        return cell
+    func listView(_ listView: ListView, viewForRowAt index: Int) -> UIView {
+        var cell = listView.dequeueReusableView(for: index) as? GameTransferringSelectAdapterCell
+        if cell == nil {
+            cell = GameTransferringSelectAdapterCell()
+        }
+        cell!.transferring = delegate.transferSelectAdapter(self, transferringAt: index)
+        cell!.current = currentIndex == index
+        return cell!
     }
+
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return delegate.numberOfItems(transferSelectAdapter: self)
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! GameTransferringSelectAdapterCell
+//        cell.transferring = delegate.transferSelectAdapter(self, transferringAt: indexPath.row)
+//        cell.current = currentIndex == indexPath.row
+//        return cell
+//    }
 }
 
-class GameTransferringSelectAdapterCell: UITableViewCell {
-    
-    @IBOutlet private weak var railwayView: GameMainRailwayView!
+class GameTransferringSelectAdapterCell: UICustomView {
     
     var transferring: Transferring! {
         didSet {
-            railwayView.update(transferring: transferring)
+            
         }
     }
     
     var current = false {
         didSet {
-            railwayView.border = current ? 4 : 1
-            railwayView.borderColor = current ? .red : .gray
+            
         }
     }
 }
--- Itadaki/Sources/View/ListView.swift
+++ Itadaki/Sources/View/ListView.swift
@@ -4,6 +4,37 @@
 // =============================================================================
 import UIKit
 
-class ListView: UICustomView {
+@objc protocol ListViewDatasource: NSObjectProtocol {
     
+    func numberOfRows(in listView: ListView) -> Int
+    
+    func listView(_ listView: ListView, viewForRowAt index: Int) -> UIView
+}
+
+@objc protocol ListViewDelegate: NSObjectProtocol {
+    
+}
+
+class ListView: UIView {
+    
+    @IBOutlet weak var dataSource: UITableViewDataSource?
+    @IBOutlet weak var delegate: UITableViewDelegate?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        reloadData()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        reloadData()
+    }
+    
+    func reloadData() {
+        
+    }
+    
+    func dequeueReusableView(for: Int) -> UIView? {
+        return nil
+    }
 }
