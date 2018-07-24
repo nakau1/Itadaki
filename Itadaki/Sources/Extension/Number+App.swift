b5de83a2bae387734efe2c66028be46cb35b5b63
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2018-07-24 20:13:48 +0900

    数字系のextension追加

---
+++ Itadaki/Sources/Extension/Number+App.swift
@@ -0,0 +1,12 @@
+// =============================================================================
+//  Itadaki
+//  Copyright Yuichi Nakayasu. All rights reserved.
+// =============================================================================
+import UIKit
+
+extension Bool {
+    
+    var reversed: Bool {
+        return self ? false : true
+    }
+}
